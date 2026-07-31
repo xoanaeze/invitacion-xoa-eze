b324511b73caccbe39b0907678d137a3d1c2a5de
xoanaeze <Xoanamayala@gmail.com>
2026-07-31 14:34:02 -0300

    Add function to open an envelope

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// ===== Preparación del sobre =====
 
+let sobreAbierto = false;
+
+function abrirSobre(){
+    if(sobreAbierto) return;
+
+    sobreAbierto = true;
+
+    console.log("El sobre se abrirá aquí.");
+}
